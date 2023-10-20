8f8e4e960cbf0b07d6ce15c3710242976749fc5e
Juan Carlos Barrera Barrera <51795550+Juancarlos56@users.noreply.github.com>
2023-10-20 11:22:23 -0500

    delete project

--- 01-typescript-intro/src/bases/01-types.ts
+++
@@ -1,4 +0,0 @@
-export const name = 'Juan';
-export const age: number = 25;
-export const isValid: boolean = true;
-export const templateString = ` Esto es un string multilinea "" '' e inyectar valores ${name}`;
